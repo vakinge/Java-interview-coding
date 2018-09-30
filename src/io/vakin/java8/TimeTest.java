5b7e761d05d68534905d608deb40eaf2684274da
vakinge <vakinge@gmail.com>
2018-09-30 14:34:15 +0800

    +

---
+++ src/io/vakin/java8/TimeTest.java
@@ -0,0 +1,125 @@
+package io.vakin.java8;
+
+import java.time.Clock;
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Locale;
+
+public class TimeTest {
+
+	public static void main(String[] args) {
+        localDate();
+        localDateTime();
+        localTime();
+
+    }
+
+	private static void localTime() {
+		Clock clock = Clock.systemDefaultZone();
+        long t0 = clock.millis();
+        System.out.println(t0);
+
+        Instant instant = clock.instant();
+        Date legacyDate = Date.from(instant);
+
+
+        ZoneId zone1 = ZoneId.of("Europe/Berlin");
+        ZoneId zone2 = ZoneId.of("Brazil/East");
+
+        System.out.println(zone1.getRules());
+        System.out.println(zone2.getRules());
+
+        // time
+        LocalTime now1 = LocalTime.now(zone1);
+        LocalTime now2 = LocalTime.now(zone2);
+
+        System.out.println(now1);
+        System.out.println(now2);
+
+        System.out.println(now1.isBefore(now2));  // false
+
+        long hoursBetween = ChronoUnit.HOURS.between(now1, now2);
+        long minutesBetween = ChronoUnit.MINUTES.between(now1, now2);
+        System.out.println(hoursBetween);
+        System.out.println(minutesBetween);
+
+
+        // create time
+
+        LocalTime now = LocalTime.now();
+        System.out.println(now);
+
+        LocalTime late = LocalTime.of(23, 59, 59);
+        System.out.println(late);
+
+        DateTimeFormatter germanFormatter =
+                DateTimeFormatter
+                        .ofLocalizedTime(FormatStyle.SHORT)
+                        .withLocale(Locale.GERMAN);
+
+        LocalTime leetTime = LocalTime.parse("13:37", germanFormatter);
+        System.out.println(leetTime);
+	}
+
+	private static void localDateTime() {
+		LocalDateTime sylvester = LocalDateTime.of(2014, Month.DECEMBER, 31, 23, 59, 59);
+
+        DayOfWeek dayOfWeek = sylvester.getDayOfWeek();
+        System.out.println(dayOfWeek);      // WEDNESDAY
+
+        Month month = sylvester.getMonth();
+        System.out.println(month);          // DECEMBER
+
+        long minuteOfDay = sylvester.getLong(ChronoField.MINUTE_OF_DAY);
+        System.out.println(minuteOfDay);    // 1439
+
+        Instant instant = sylvester
+                .atZone(ZoneId.systemDefault())
+                .toInstant();
+
+        Date legacyDate = Date.from(instant);
+        System.out.println(legacyDate);     // Wed Dec 31 23:59:59 CET 2014
+
+
+        DateTimeFormatter formatter =
+                DateTimeFormatter
+                        .ofPattern("MMM dd, yyyy - HH:mm");
+
+        LocalDateTime parsed = LocalDateTime.parse("Nov 03, 2014 - 07:13", formatter);
+        String string = parsed.format(formatter);
+        System.out.println(string);
+	}
+
+	private static void localDate() {
+		LocalDate today = LocalDate.now();
+        LocalDate tomorrow = today.plus(1, ChronoUnit.DAYS);
+        LocalDate yesterday = tomorrow.minusDays(2);
+
+        System.out.println(today);
+        System.out.println(tomorrow);
+        System.out.println(yesterday);
+
+        LocalDate independenceDay = LocalDate.of(2014, Month.JULY, 4);
+        DayOfWeek dayOfWeek = independenceDay.getDayOfWeek();
+        System.out.println(dayOfWeek);    // FRIDAY
+
+        DateTimeFormatter germanFormatter =
+                DateTimeFormatter
+                        .ofLocalizedDate(FormatStyle.MEDIUM)
+                        .withLocale(Locale.GERMAN);
+
+        LocalDate xmas = LocalDate.parse("24.12.2014", germanFormatter);
+        System.out.println(xmas);   // 2014-12-24
+	}
+
+}
